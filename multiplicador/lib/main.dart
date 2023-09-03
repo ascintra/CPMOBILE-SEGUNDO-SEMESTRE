d442cb1a8c82a24d0e7acaade1cd4e4327afba7a
André Spinelli Cintra <142934117+ascintra@users.noreply.github.com>
2023-09-03 19:24:27 -0300

    Update main.dart

--- multiplicador/lib/main.dart
+++ multiplicador/lib/main.dart
@@ -1,81 +1,122 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MeuApp());
+void main() {
+  runApp(MeuApp());
+}
 
 class MeuApp extends StatelessWidget {
-  const MeuApp({super.key});
-  static const String _titulo = 'Contador de Cliques';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _titulo,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Calculadora(),
+        ),
       ),
-      home: const MinhaPaginaInicial(),
     );
   }
 }
 
-class MinhaPaginaInicial extends StatefulWidget {
-  const MinhaPaginaInicial({super.key});
-
+class Calculadora extends StatefulWidget {
   @override
-  _EstadoMinhaPaginaInicial createState() => _EstadoMinhaPaginaInicial();
+  _EstadoCalculadora createState() => _EstadoCalculadora();
 }
 
-class _EstadoMinhaPaginaInicial extends State<MinhaPaginaInicial> {
-  int _contador = 0;
+class _EstadoCalculadora extends State<Calculadora> {
+  TextEditingController controlador1 = TextEditingController();
+  TextEditingController controlador2 = TextEditingController();
+  String resultado = "";
 
-  void _incrementarContador() {
+  void calcular() {
     setState(() {
-      _contador++;
-    });
-  }
+      try {
+        double num2 = double.parse(controlador2.text);
+        String operacao = controlador1.text.trim();
+        double num1 = double.parse(operacao.substring(0, operacao.length - 1));
+        String operador = operacao.substring(operacao.length - 1);
 
-  void _decrementarContador() {
-    setState(() {
-      if (_contador > 0) {
-        // Verificando se o contador é maior que zero
-        _contador--;
+        switch (operador) {
+          case '+':
+            resultado = (num1 + num2).toString();
+            break;
+          case '-':
+            resultado = (num1 - num2).toString();
+            break;
+          case '*':
+            resultado = (num1 * num2).toString();
+            break;
+          case '/':
+            if (num2 != 0) {
+              resultado = (num1 / num2).toString();
+            } else {
+              resultado = "Divisão por zero";
+            }
+            break;
+          default:
+            resultado = "Operador inválido";
+        }
+      } catch (e) {
+        resultado = "Expressão inválida";
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Contador de Cliques em Flutter'),
-      ),
-      body: Center(
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 2),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Você clicou esse número de vezes:',
+          children: [
+            Container(
+              width: 300,
+              child: TextField(
+                controller: controlador1,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  hintText: 'Insira o valor e operador +,-,*,/',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
             ),
+            SizedBox(height: 20),
+            Container(
+              width: 300,
+              child: TextField(
+                controller: controlador2,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  hintText: 'Insira o segundo valor',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: calcular,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                onPrimary: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              child: Text('Calcular'),
+            ),
+            SizedBox(height: 20),
             Text(
-              '$_contador',
-              style: const TextStyle(fontSize: 25),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FloatingActionButton(
-                  onPressed: _decrementarContador,
-                  tooltip: 'Decrementar',
-                  backgroundColor: Colors.green,
-                  child: const Icon(Icons.remove),
-                ),
-                SizedBox(width: 20), // Espaço entre os botões
-                FloatingActionButton(
-                  onPressed: _incrementarContador,
-                  tooltip: 'Incrementar',
-                  child: const Icon(Icons.add),
-                ),
-              ],
+              'Resultado: $resultado',
+              style: TextStyle(fontSize: 24),
             ),
           ],
         ),
